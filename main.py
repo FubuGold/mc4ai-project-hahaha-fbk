1a671f64a037b035d62dbda663504ccbb00baa28
FubuGold <trangiahuy211207@gmail.com>
2023-04-17 17:23:54 +0700

    too.much.commit

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def main():
     with DataTab:
         Data = pd.read_csv('py4ai-score.csv')
         Data.drop(['NAME','GENDER','CLASS'],axis='columns',inplace=True)
-        st.dataframe(data=Data)
-    pass
+        st.dataframe(data=Data, height=1000)
+
 
 if __name__ == '__main__':
     main()
