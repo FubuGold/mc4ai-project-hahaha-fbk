68da0237ee8b0ceba91c04270c0ab6025e950f11
FubuGold <trangiahuy211207@gmail.com>
2023-04-17 17:11:46 +0700

    help

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 def main(): 
     st.title('Phân tích và xem điểm Python4AI 092022')
+    
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
     with PersonalInfoTab:
         st.header('Login')
