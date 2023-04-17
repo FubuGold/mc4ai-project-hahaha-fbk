fb32b7d347ebebbd23754c289132a6bd9f0939eb
FubuGold <trangiahuy211207@gmail.com>
2023-04-17 15:45:43 +0700

    file main sườn code

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+def main():
+    PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
+    with PersonalInfoTab:
+        st.header('Login')
+    with AnalyzeTab:
+        st.header('Phân tích dữ liệu')
+    with DataTab:
+        Data = pd.read_csv('data.csv')
+        Data.drop(['NAME','GENDER','CLASS'],axis='columns',inplace=True)
+        st.dataframe(data=Data)
+    pass
+
+if __name__ == '__main__':
+    main()
