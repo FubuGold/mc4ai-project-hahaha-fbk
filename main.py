8e940ae17fda27a80101f34a4f012b329285c8c2
FubuGold <trangiahuy211207@gmail.com>
2023-04-17 17:14:18 +0700

    ok

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 import streamlit as st
 import pandas as pd
-def main():
-    PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
-    with PersonalInfoTab:
-        st.header('Login')
-    with AnalyzeTab:
-        st.header('Phân tích dữ liệu')
-    with DataTab:
-        Data = pd.read_csv('py4ai-score.csv')
-        Data.drop(['NAME','GENDER','CLASS'],axis='columns',inplace=True)
-        st.dataframe(data=Data, height=1000)
-    pass
-import streamlit as st
-import pandas as pd
 def main(): 
     st.title('Phân tích và xem điểm Python4AI 092022')
     
-
-if __name__ == '__main__':
-    main()
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
     with PersonalInfoTab:
         st.header('Login')
