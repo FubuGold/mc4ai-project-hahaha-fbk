39feffdc267cb45e609240916cd58e29c112f677
FubuGold <trangiahuy211207@gmail.com>
2023-04-24 15:09:22 +0700

    debug

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ def main():
     with AnalyzeTab:
         st.header('Phân tích dữ liệu')
     with DataTab:
-        SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
-        SubData_Point.reset_index(drop=True)
-        st.dataframe(data=SubData_Point, height=1000)
-        st.dataframe(data=Data[['NAME','CLASS']], height=1000)
-        
+        col1,col2 = st.columns([50,10])
+        with col1:
+            SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
+            SubData_Point.reset_index(drop=True)
+            st.dataframe(data=SubData_Point, height=1000)
+        with col2:
+            st.dataframe(data=Data[['NAME','CLASS']], height=1000)
+
 
 
 
