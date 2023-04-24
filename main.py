6d97714ae061547395799467e878a6b786caeb89
FubuGold <trangiahuy211207@gmail.com>
2023-04-24 15:06:21 +0700

    password col

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import pandas as pd
 def main(): 
     st.set_page_config(layout="wide")
     st.title('Phân tích và xem điểm Python4AI 092022')
-    
+    Data = pd.read_csv('py4ai-score.csv')
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
     with PersonalInfoTab:
         st.header('Login')
     with AnalyzeTab:
         st.header('Phân tích dữ liệu')
     with DataTab:
-        Data = pd.read_csv('py4ai-score.csv').sample(frac=1, random_state=69)
-        Data.drop(['NAME','GENDER','CLASS'],axis='columns',inplace=True)
-        st.dataframe(data=Data, height=1000)
+        SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
+        SubData_Point.reset_index(drop=True)
+        st.dataframe(data=SubData_Point, height=1000)
+        st.dataframe(data=Data[['NAME','CLASS']], height=1000)
+        
+
 
 
 if __name__ == '__main__':
