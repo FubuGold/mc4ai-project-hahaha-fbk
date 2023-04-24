04da29d686d09fef60267b9f67b0e6f7cde4ae1b
FubuGold <trangiahuy211207@gmail.com>
2023-04-24 15:23:00 +0700

    debug 3

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ def main():
     with DataTab:
         col1,col2 = st.columns(2)
         with col1:
+            st.header('Danh sách điểm')
+            st.write('Đã xáo trộn ngẫu nhiên')
             SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
             SubData_Point.reset_index(drop=True)
             st.dataframe(data=SubData_Point, height=1000)
         with col2:
-            st.dataframe(data=Data[['NAME','CLASS']], height=1000)
+            st.header('Danh sách username, password mặc định')
+            pwd_df = pd.DataFrame({
+                'Username': Data[['NAME','CLASS']].apply(''.join, axis=1),
+                'Password': ['123456789' for _ in range(len(Data))] 
+            })
+            st.dataframe(data=pwd_df, height=1000)
 
 
 
