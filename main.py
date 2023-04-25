b219d109c8817b5b19425d2c9bb70086934a0df0
FubuGold <trangiahuy211207@gmail.com>
2023-04-25 15:28:27 +0700

    remove username,password

--- main.py
+++ main.py
@@ -8,23 +8,16 @@ def main():
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
     with PersonalInfoTab:
         st.header('Login')
+        
     with AnalyzeTab:
         st.header('Phân tích dữ liệu')
+
     with DataTab:
-        col1,col2 = st.columns(2)
-        with col1:
-            st.header('Danh sách điểm')
-            st.write('Đã xáo trộn ngẫu nhiên')
-            SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
-            SubData_Point.reset_index(drop=True)
-            st.dataframe(data=SubData_Point, height=1000)
-        with col2:
-            st.header('Danh sách username, password mặc định')
-            pwd_df = pd.DataFrame({
-                'Username': Data['NAME'] + Data['CLASS'],
-                'Password': ['123456789' for _ in range(len(Data))] 
-            })
-            st.dataframe(data=pwd_df, height=1000)
+        st.header('Danh sách điểm')
+        st.write('Đã xáo trộn ngẫu nhiên')
+        SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
+        SubData_Point.reset_index(drop=True)
+        st.dataframe(data=SubData_Point, height=1000)
 
 
 
