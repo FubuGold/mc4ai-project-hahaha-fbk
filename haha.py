f11fa78359c304785fb286ab316442efc9187817
smile211107 <smile211107@gmail.com>
2023-04-26 22:31:31 +0700

    haizzzz

--- haha.py
+++ haha.py
@@ -1,6 +1,25 @@
 import plotly.express as px
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import streamlit as st
 def phan_bo_diem(data, s):
-    px.histogram(data, x = s)
+    st.plotly_chart(px.histogram(data, x = s, color = "GENDER"))
 def analyze(data):
     for i in range(10):
-        phan_bo_diem(data, "S" + str(i+1))
+        phan_bo_diem(data, 'S' + str(i+1))
+    st.plotly_chart(px.pie(data, names ='GENDER'))
+    st.plotly_chart(px.pie(data, names ='PYTHON-CLASS'))
+    ##########################################################
+    for i in range(10):
+        temp = [float(j) for j in data['S'+ str(i+1)]]
+        data['S'+ str(i+1)] = temp
+    temp = [np.array(data['S'+ str(i+1)]).mean() for i in range(10)]
+    index = [i for i in range(1,11)]
+    d1 = pd.DataFrame({'avg_score' : temp,
+                       'index'     : index})
+    n_bins = 20
+    plt.hist(d1['avg_score'], n_bins, density = True, 
+         histtype ='bar')
+    plt.bar(x = d1['index'], y = d1['avg_score'])
+    plt.show()
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
+from haha import *
 def main(): 
+    Data = pd.read_csv('py4ai-score.csv')
+    Data.drop(['NAME','CLASS'],axis='columns',inplace=True)
     st.set_page_config(layout="wide")
     st.title('Phân tích và xem điểm Python4AI 092022')
-    
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
     with PersonalInfoTab:
         st.header('Login')
     with AnalyzeTab:
         st.header('Phân tích dữ liệu')
+        analyze(Data)
     with DataTab:
-        Data = pd.read_csv('py4ai-score.csv')
-        Data.drop(['NAME','GENDER','CLASS'],axis='columns',inplace=True)
         st.dataframe(data=Data, height=1000)
-
 
 if __name__ == '__main__':
     main()
