da0b86b2e2860098d7389116f150b9a143a05e53
FubuGold <trangiahuy211207@gmail.com>
2023-05-01 18:26:17 +0700

    Password manager update 1

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import streamlit as st
 import pandas as pd
-from PersonalInfoManager import ComparePredictReal
+from PersonalInfoShowcase import Login_Personal_Tab
 
 @st.cache_data
 def ReadData():
     return pd.read_csv('py4ai-score.csv')
-
-def PersonalInfoShowcase(Data, UserID):
-    model = ComparePredictReal(Data, Data.iloc[UserID])
-    st.markdown('## Điểm cá nhân')
-    st.table(Data.iloc[UserID])
-    st.markdown('## Dự đoán bằng AI Regression')
-    st.table(model.CompareTable())
-    st.markdown("### **Nhận xét:**")
-    st.markdown('Giữa **trung bình điểm homework**, **midterm exam** và **final exam** không có tính liên kết chặt chẽ, thể hiện qua độ chính xác của AI')
 
 def main(): 
     st.set_page_config(layout="wide")
@@ -22,9 +13,7 @@ def main():
     PersonalInfoTab, AnalyzeTab, DataTab = st.tabs(['Xem điểm','Phân tích thống kê','Bảng điểm tổng quát'])
 
     with PersonalInfoTab:
-        placeholder = st.empty()
-        with placeholder.container():
-            PersonalInfoShowcase(Data,105)
+        Login_Personal_Tab(Data)
         
     with AnalyzeTab:
         st.header('Phân tích dữ liệu')
@@ -35,10 +24,6 @@ def main():
         SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
         SubData_Point.reset_index(drop=True)
         st.dataframe(data=SubData_Point, height=1000)
-
-
-
-
 
 if __name__ == '__main__':
     main()
