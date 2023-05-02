d23cec7b49a730447563f6ae88ef6c144163c104
FubuGold <trangiahuy211207@gmail.com>
2023-05-02 15:26:15 +0700

    danh sách username/password

--- main.py
+++ main.py
@@ -19,11 +19,19 @@ def main():
         st.header('Phân tích dữ liệu')
 
     with DataTab:
-        st.header('Danh sách điểm')
-        st.write('Đã xáo trộn ngẫu nhiên')
-        SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
-        SubData_Point.reset_index(drop=True)
-        st.dataframe(data=SubData_Point, height=1000)
+        col1,col2 = st.columns(2)
+        with col1:
+            st.header('Danh sách điểm')
+            st.write('Đã xáo trộn ngẫu nhiên')
+            SubData_Point = Data.drop(['NAME','GENDER','CLASS'],axis='columns').sample(frac=1, random_state=69)
+            SubData_Point.reset_index(drop=True)
+            st.dataframe(data=SubData_Point, height=1000)
+
+        with col2:
+            st.header('Danh sách username/password mặc định')
+            SubData = Data['NAME'] + Data.index
+            st.dataframe(data=SubData, height=1000)
+
 
 if __name__ == '__main__':
     main()
